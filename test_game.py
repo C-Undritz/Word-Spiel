6cec62c277d0939875a4f69041e72bcd6d63f37f
C-Undritz <undritz.cu@gmail.com>
2022-06-21 20:30:02 +0000

    Added more tests to 'test_result_current_results'

--- test_game.py
+++ test_game.py
@@ -8,6 +8,8 @@ from game_logic import determine_results
 @pytest.mark.parametrize("word,answer,expected", [
     ('hello', 'hello', {'hello': ['3', '3', '3', '3', '3']}),
     ('ready', 'early', {'early': ['2', '2', '2', '1', '3']}),
+    ('teeth', 'reach', {'reach': ['1', '3', '1', '1', '3']}),
+    ('teeth', 'teach', {'teach': ['3', '3', '1', '1', '3']}),
 ])
 def test_result_current_results(word, answer, expected):
     """

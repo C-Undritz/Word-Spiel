9d09195deb2545d5c95fb7f6a6cbee58e109f350
C-Undritz <undritz.cu@gmail.com>
2022-06-21 12:35:07 +0000

    Added play_again() to reset class and added reset class to home()

--- app.py
+++ app.py
@@ -10,24 +10,26 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 rapidapi_key = os.environ.get("RAPIDAPI_KEY")
-game = Game(0, [])
+game = Game(0, [], False)
 
 
 @app.route("/")
 @app.route("/home")
 def home():
     """
-    Displays the start screen
+    Displays the start screen.  Resets the instantiated Game class should the
+    user quit to the start screen during a game.
     """
+    game.reset()
     return render_template("index.html")
 
 
 @app.route("/games_screen", methods=["GET", "POST"])
 def start_game():
     """
-    Processes the submitted word per guess
+    Processes the submitted answer against the word and updates required Game
+    class attributes as required.
     """
-    print(game.word)
     if request.method == "POST":
         answer = ""
         i = 1
@@ -38,10 +40,22 @@ def start_game():
         round_results = determine_results(game.word, answer)
         game.add_results(round_results)
         game.increment_count(1)
+        game.determine_win(round_results.win)
 
         return render_template("game_screen.html", game=game)
 
     return render_template("game_screen.html", game=game)
+
+
+@app.route("/play_again", methods=["GET", "POST"])
+def play_again():
+    '''
+    Resets the game should the player select to play again and directs to the
+    game screen.
+    '''
+    if request.method == "POST":
+        game.reset()
+        return redirect(url_for("start_game"))
 
 
 if __name__ == "__main__":

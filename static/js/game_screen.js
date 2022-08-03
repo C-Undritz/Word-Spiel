3e67c3203f339f0563bb74d6c28ee2f78d4f8e68
C-Undritz <undritz.cu@gmail.com>
2022-08-03 09:12:36 +0000

    Added required JS for modal and adjusted comments

--- static/js/game_screen.js
+++ static/js/game_screen.js
@@ -1,6 +1,8 @@
 /*
-Moves the cursor to the next box once a character has been entered.  Ignores certain keys to improve the 
-navigation between boxes and therefore the user experience.
+-----------------------------------------------------------------------------------------------------------
+JS required to moves the cursor to the next box once a character has been entered.  Ignores certain keys to 
+improve the navigation between boxes and therefore the user experience.
+-----------------------------------------------------------------------------------------------------------
 */
 
 document.getElementById("1").focus();
@@ -9,7 +11,7 @@ document.getElementById("1").focus();
 const inputBoxes = document.getElementsByClassName("char-input-box");
 const reservedKeys = ["Backspace", "Delete", "Tab", "Shift"];
 // https: //www.codegrepper.com/code-examples/javascript/frameworks/ionic/javascript+if+statement+with+time+delay
-const delay = 100; //sets delay for .5 second
+const delay = 100; //sets delay for .1 second
 
 for (let i = 0; i < inputBoxes.length; i++) {
     inputBoxes[i].addEventListener("keydown", function () {
@@ -17,8 +19,44 @@ for (let i = 0; i < inputBoxes.length; i++) {
             inputBoxes[i].focus();
         } else {
             setTimeout(function () {
-                inputBoxes[i + 1].focus();
+                if (i <= 3) { // stops console error 'Uncaught TypeError: Cannot read properties of undefined (reading 'focus')'
+                    console.log(i)
+                    inputBoxes[i + 1].focus();
+                }
             }, delay)
         }
     });
+}
+
+/*
+-----------------------------------------------------------------------------------------------------------
+JS required for the custom modal(https: //www.w3schools.com/howto/howto_css_modals.asp)
+-----------------------------------------------------------------------------------------------------------
+*/
+
+const modal = document.getElementById("myModal");
+const input = document.getElementById("char-input-form")
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
+let roundCount = document.getElementById("output-boxes").childElementCount;
+
+/*
+When five output displays are shown and now win, modal displays and input disappears so as to disable 
+further guesses.
+*/
+if (roundCount == 5) {
+    modal.style.display = "block";
+    input.style.display = "none";
+};
+
+// Closes modal when the user clicks on <span> (x).
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// Closes modal when the user clicks anywhere outside of the modal.
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }

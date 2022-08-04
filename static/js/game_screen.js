18687c5b11891c4c2b04e6cd2d4628d7ce9bd199
C-Undritz <undritz.cu@gmail.com>
2022-08-04 13:09:23 +0000

    Updated the display condition for the modal if a win achieved

--- static/js/game_screen.js
+++ static/js/game_screen.js
@@ -14,18 +14,19 @@ const reservedKeys = ["Backspace", "Delete", "Tab", "Shift"];
 const delay = 100; //sets delay for .1 second
 
 for (let i = 0; i < inputBoxes.length; i++) {
-    inputBoxes[i].addEventListener("keydown", function () {
-        if (reservedKeys.includes(event.key)) {
-            inputBoxes[i].focus();
-        } else {
-            setTimeout(function () {
-                if (i <= 3) { // stops console error 'Uncaught TypeError: Cannot read properties of undefined (reading 'focus')'
-                    console.log(i)
-                    inputBoxes[i + 1].focus();
-                }
-            }, delay)
+  inputBoxes[i].addEventListener("keydown", function () {
+    if (reservedKeys.includes(event.key)) {
+      inputBoxes[i].focus();
+    } else {
+      setTimeout(function () {
+        if (i <= 3) {
+          // stops console error 'Uncaught TypeError: Cannot read properties of undefined (reading 'focus')'
+          console.log(i);
+          inputBoxes[i + 1].focus();
         }
-    });
+      }, delay);
+    }
+  });
 }
 
 /*
@@ -35,28 +36,33 @@ JS required for the custom modal(https: //www.w3schools.com/howto/howto_css_moda
 */
 
 const modal = document.getElementById("myModal");
-const input = document.getElementById("char-input-form")
+const input = document.getElementById("char-input-form");
 const btn = document.getElementById("myBtn");
 const span = document.getElementsByClassName("close")[0];
+const win = document.getElementById("win-bool").innerHTML;
 let roundCount = document.getElementById("output-boxes").childElementCount;
 
 /*
 When five output displays are shown and now win, modal displays and input disappears so as to disable 
 further guesses.
 */
-if (roundCount == 5) {
-    modal.style.display = "block";
-    input.style.display = "none";
-};
+
+console.log(win);
+console.log(typeof win);
+
+if (roundCount == 5 || win == "True") {
+  modal.style.display = "block";
+  input.style.display = "none";
+}
 
 // Closes modal when the user clicks on <span> (x).
 span.onclick = function () {
-    modal.style.display = "none";
-}
+  modal.style.display = "none";
+};
 
 // Closes modal when the user clicks anywhere outside of the modal.
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
